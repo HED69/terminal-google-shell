098305d5b5b30be3f39f00f66d91f2c07749da28
HED69 <96006135+HED69@users.noreply.github.com>
2022-11-03 00:28:03 +0100

    Update jsjsjs.sh

--- jsjsjs.sh
+++ jsjsjs.sh
@@ -6,18 +6,18 @@ sudo apt upgrade -y
 sudo apt-get upgrade -y
 sudo apt install neofetch -y
 sudo apt install figlet -y
-sudo apt install git -y
 sudo apt install tmate -y
-sudo apt install cmatrix -y
-clear
 neofetch
+clear
+
+figlet google clound shell script
 echo "bienvenido"
 echo "este script instalara todo lo necesario para poder usar google clound shell como terminal"
 echo "la intencion es que este script lo haga todo de manera automatica"
-echo "esto es de uso privado, asi que no lo compartas con nadi"
-echo "autor del script: HED"
 echo "despues de unos segundos se iniciara un ssh"
-echo "tendras que hacer captura de pantalla o copiar y pegar el ssh y el enlace"
-echo "en caso de que hallas iniciado este script sin querer pulsa ctrl+c para detenerlo"
-sleep 10
+echo "se estara usando tmate como ssh debido a la facilidad de uso"
+echo "tendras que poner el ssh en otra terminal con el comando: ssh (codigo)"
+echo "para detener el script pulsa ctrl+c"
+echo "creador: HED"
+sleep 15
 tmate
