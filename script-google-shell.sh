e8861b5a505fc398bce89f2bcd8f9142a8d91969
HED69 <96006135+HED69@users.noreply.github.com>
2022-11-04 12:09:01 +0100

    Update script-google-shell.sh

--- script-google-shell.sh
+++ script-google-shell.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-sudo apt update 
+sudo apt update
 sudo apt-get update
 sudo apt upgrade -y
 sudo apt-get upgrade -y
 sudo apt install neofetch -y
 sudo apt install figlet -y
 sudo apt install tmate -y
-sudo apt install vim -y
-sudo apt install emacs -y
-sudo apt install openssh -y
+sudo apt install nmap -y
+sudo apt update
+sudo apt upgrade -y
 clear
 
 figlet google clound shell script
-echo "bienvenido"
-echo "este script a instalada todo lo necesario para poder usar google clound shell como terminal"
-echo "la intencion es que este script lo haga todo de manera automatica"
-echo "despues de unos segundos se iniciara un ssh"
-echo "se estara usando tmate como ssh debido a la facilidad de uso"
-echo "tendras que poner el ssh en otra terminal con el comando: ssh (codigo)"
-echo "en caso de que no quieras usar el ssh escribes exit"
-echo "para detener el script pulsa ctrl+c"
-echo "creador: HED"
+echo "welcome"
+echo "this script will install everything you need to be able to use google clound shell as a terminal"
+echo "the intention is that this script does everything automatically"
+echo "after a few seconds an ssh will start"
+echo "you will be using tmate as ssh due to ease of use"
+echo "you will have to put the ssh in another terminal with the command: ssh (code)"
+echo "to stop the script press ctrl+c"
+echo "creator: HED"
 sleep 15
+clear
+figlet "charging..."
+sleep 5
 tmate
+
