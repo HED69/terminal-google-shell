c2e9379fd223f1f9f545963cd9de98e537fa1051
HED <96006135+HED69@users.noreply.github.com>
2022-11-18 10:48:43 +0100

    Add files via upload

---
+++ pawns-script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt update 
+sudo apt upgrade -y
+clear
+echo "a script will be executed that will allow you to generate money by wifi from google cloud"
+sleep 2
+sudo apt install git -y
+clear
+cd
+cd terminal-google-shell
+ls
+chmod u+x pawns-cli
+echo "Now you can run pawns.app, you can run it with the command below"
+sleep 1
+echo "You will have to create an account at: https://pawns.app?r=1036352"
+sleep 1
+echo "./pawns-cli -email=example@example.com -password=example -device-name=example -accept-tos"
+
